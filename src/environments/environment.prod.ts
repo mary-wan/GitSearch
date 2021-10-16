d7990f67210b8eb31d227af045c7d70de7a13032
mary-wan <mary.njenga@student.moringaschool.com>
2021-10-16 17:39:42 +0300

    gitignore is now working

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl:"https://api.github.com/users/",
-  ApiKey: "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",
-  clientid:'8f8ca1b9f46944953b221e93ba90793030b38de9',
-  clientsecret: ' a0491ce4b9bad3f63000a99ef95848ea54d06694',
-  apiKey: '?client_id 8f8ca1b9f46944953b221e93ba90793030b38de9&client_secret=a0491ce4b9bad3f63000a99ef95848ea54d06694'
-};
