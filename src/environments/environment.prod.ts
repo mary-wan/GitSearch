a1f1dd339b9deb31e9b2a10811d844fe501cd9d7
mary-wan <mary.njenga@student.moringaschool.com>
2021-10-16 17:01:24 +0300

    add client id

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:"https://api.github.com/users/",
+  ApiKey: "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",
+  clientid:'8f8ca1b9f46944953b221e93ba90793030b38de9',
+  clientsecret: ' a0491ce4b9bad3f63000a99ef95848ea54d06694'
 };
